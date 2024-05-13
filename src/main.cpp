0f33cde9eae5334c2a95ec647be913661d4aabb0
Nico van Ommen <nicovanommen.nvo@gmail.com>
2024-05-13 23:14:19 +0200

    Fixed readvertising issues

--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,16 @@
 #include <BLEUtils.h>
 
 
-#define BLE_SERVER_NAME "BLE-ESP32-Beacon"
-#define SERVICE_UUID    "4ff10849-b57e-4761-a6ac-99d00ee5e5c6"
+#define BLE_SERVER_NAME         "BLE-ESP32-Beacon"
+#define SERVICE_UUID            "4ff10849-b57e-4761-a6ac-99d00ee5e5c6"
+#define CHARACTERISTICS_UUID    "9a8d7af1-8136-4549-b3f7-d8529ab98ba6"
 
 
-BLEScan     *pBLEScan;
+BLEScan *pBLEScan;
 bool deviceConnected = false;
 
 
-BLECharacteristic BeaconCharacteristics("9a8d7af1-8136-4549-b3f7-d8529ab98ba6", BLECharacteristic::PROPERTY_NOTIFY);
+BLECharacteristic BeaconCharacteristics(CHARACTERISTICS_UUID, BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE);
 BLEDescriptor BeaconDescriptor(BLEUUID((uint16_t)0x2902));
 
 
@@ -27,6 +28,8 @@ class BeaconServerCallbacks: public BLEServerCallbacks
     void onDisconnect(BLEServer *pServer)
     {
         deviceConnected = false;
+
+        pServer->getAdvertising()->start();
     }
 };
 
@@ -34,7 +37,6 @@ class BeaconServerCallbacks: public BLEServerCallbacks
 void setup()
 {
     Serial.begin(115200);
-
     BLEDevice::init(BLE_SERVER_NAME);
 
     // Setup the ble server
@@ -50,6 +52,9 @@ void setup()
     // Start advertising the server
     BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
     pAdvertising->addServiceUUID(SERVICE_UUID);
+    pAdvertising->setScanResponse(true);
+    pAdvertising->setMinPreferred(0x06);
+
     pServer->getAdvertising()->start();
     Serial.println("Iemand moet hier verbinden lol!");
 }
